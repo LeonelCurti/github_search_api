754515ec061f38a4cf6048d5085ba78f45678806
Leonel Curti <leonelcurti@gmail.com>
2018-05-16 20:32:10 -0300

    minor corrections and close project

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ searchUser.addEventListener('keyup',(e)=>{
       } else {
         //show profile
         ui.showProfile(data.profile);
-        
+        ui.showRepos(data.repos);
       }
     });
   } else{
--- github.js
+++ github.js
@@ -2,13 +2,21 @@ class GitHub {
   constructor(){
     this.client_id = '6b411f00c015d857aa9e';
     this.client_secret = 'a8bdeb59fe1c87139e2ab0bdd94d16326c19369a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
-  async getUser(user){
+  async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     }
-  } 
+  }
 }
